f7e8f1bbac9471061324acf87ccdf759a76a0c1d
raufer <raufer92@gmail.com>
2021-02-23 16:58:33 +0000

    changed lr

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ config = {
     'num-epochs-pretrain': 6,
     'num-epochs-train': 6,
     'learning-rate-pretrain': 9e-4,
-    'learning-rate-train': 2e-5,
+    'learning-rate-train': 9e-6,
     'dropout-ratio': 0.4
 }
